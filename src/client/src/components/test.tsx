a8df5ebd009566b16b6d2fdbad41b8414b60d571
Vincent <vindao-development@outlook.com>
2019-09-05 21:05:14 +0200

    changes

--- src/client/src/components/test.tsx
+++ src/client/src/components/test.tsx
@@ -3,10 +3,6 @@ import * as React from "react";
 export interface TestProps {}
 
 const Test: React.SFC<TestProps> = () => {
-  fetch("http://localhost:5000/api/example")
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
   return <h1>I Am the tester, testing this app</h1>;
 };
 
