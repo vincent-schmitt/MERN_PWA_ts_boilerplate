7698023d0dc33f287c84b3541652481aaee1e4a2
Vincent <vindao-development@outlook.com>
2019-09-06 19:31:48 +0200

    changes

--- src/client/src/Pages/Home/index.tsx
+++ src/client/src/Pages/Home/index.tsx
@@ -5,7 +5,9 @@ import { Container } from "../../Theme/custom/Container";
 export interface HomeProps {}
 
 const Home: React.SFC<HomeProps> = () => {
-  return <Container>This is the Home Container</Container>;
+  return (
+    <Container style={{ textAlign: "center" }}>This is the Home Page</Container>
+  );
 };
 
 export default Home;
